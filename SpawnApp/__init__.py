5adda14084b1696149e404ced9a69a575fe5abbb
Shane Hutter <shane.hutter86@gmail.com>
2019-03-10 13:27:52 +1100

    Early groundwork
    Some early files for laying the projects groundwork.

---
+++ SpawnApp/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+spawnapp
+    0.0.0-1
+
+    Written By:
+        Shane Hutter
+
+    The __init__ for methods packaged with spawnapp.
+"""
+
+PROJECT = {
+        "name"          : "spawnapp.py"                                     ,
+        "description"   : "Spawn a python virtual environmant application"  ,
+        "debug"         : True                                              ,
+        }
+PARSER = {
+        "name"          : PROJECT[ "name " ]        ,
+        "description"   : PROJECT[ "description" ]  ,
+        }
